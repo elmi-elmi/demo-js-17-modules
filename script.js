9b4a55ce47b63903919f3f3d885f5cb12ec536c8
shahrokh <shahrokhelmi@gmail.com>
2022-01-29 17:54:42 +0330

    nmp

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-import {addToCart}from './shopingCart';
+import { addToCart } from "./shopingCart.js";
+console.log("script");
+// addToCart('bread',2)
+//
+// console.log('before await')
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+//
+// console.log(data)
+// console.log('after await')
 
-addToCart()
+// const getLastPost = async function(){
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data = await res.json();
+//
+//     return  {tilte:data.at(-1).title, text:data.at(-1).body}
+//
+// }
+
+// const lastPost = await getLastPost();
+//
+// console.log(lastPost)
+//
+// const lastPost = getLastPost();
+// lastPost.then(last=>console.log(last))
+
+// const lastPost = getLastPost();
+// const d = await Promise.resolve(lastPost)
+// console.log('---',d)
+// const a = 2;
+// const shoppingCart2 = (function () {
+
+//   const card = [];
+//   const shoppingCost = 20;
+//   const x = 123;
+//
+//   const addToCart = function (product, quantity) {
+//     console.log("add to cart", product, quantity);
+//   };
+//
+//   const orderStock = function (product, quantity) {
+//     console.log("order stock", product, quantity);
+//   };
+//
+//   return { addToCart, card, x };
+// })();
+//
+// shoppingCart2.addToCart('hi', 11111);
+
+
--- shopingCart.js
+++ shopingCart.js
@@ -1,4 +1,12 @@
 
+console.log('shopping cart')
+console.log('start fetching user');
+
+const res = await fetch('https://jsonplaceholder.typicode.com/users');
+console.log('finish')
+
+
 export const addToCart = function(product, quantity){
     console.log(product,quantity)
 }
+
